fb8c2c9a8e0b8e2cbf84532deacb8f36d214f218
Pokesyoum <syoumpm@gmail.com>
2024-09-01 17:24:53 +0900

    items/new.html.erbにおけるform関連の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      ender :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
