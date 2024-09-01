08fcf3ac467869f07969c560635c5c889aea1808
Pokesyoum <syoumpm@gmail.com>
2024-09-01 16:40:53 +0900

    データベースオプションの追記

--- db/migrate/20240901061632_create_items.rb
+++ db/migrate/20240901061632_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.references :user
-      t.string :item_name
-      t.text :description
-      t.integer :category_id
-      t.integer :status_id
-      t.integer :fee_id
-      t.integer :area_id
-      t.integer :day_id
-      t.integer :price
+      t.references :user,     null: false, foreign_key: true
+      t.string :item_name,    null: false
+      t.text :description,    null: false
+      t.integer :category_id, null: false
+      t.integer :status_id,   null: false
+      t.integer :fee_id,      null: false
+      t.integer :area_id,     null: false
+      t.integer :day_id,      null: false
+      t.integer :price,       null: false
       t.timestamps
     end
   end
