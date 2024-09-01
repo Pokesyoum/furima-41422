16f247a4899093de667881570dce50da912d481b
Pokesyoum <syoumpm@gmail.com>
2024-09-01 14:50:44 +0900

    レビュー後修正2（正規表現にーを追加）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }
   validates :last_name,  presence: true,
-                         format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'must be in full-width characters (Kanji, Hiragana, Katakana)' }
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be in full-width characters (Kanji, Hiragana, Katakana)' }
   validates :first_name, presence: true,
-                         format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'must be in full-width characters (Kanji, Hiragana, Katakana)' }
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be in full-width characters (Kanji, Hiragana, Katakana)' }
   validates :last_kana,  presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be in full-width characters (Katakana)' }
   validates :first_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be in full-width characters (Katakana)' }
   validates :birthday,   presence: true
