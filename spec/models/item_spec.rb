a52916b15fa4aac5998949acdc1e6e5a544a8139
Pokesyoum <syoumpm@gmail.com>
2024-09-03 21:22:24 +0900

    異常系テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,5 +11,56 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
     end
+    context '商品を出品できないとき' do
+      it 'userが紐づいていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'User must exist'
+      end
+      it 'categoryが空だと出品できない' do
+        @item.category = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+      it 'statusが空だと出品できない' do
+        @item.status = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Status can't be blank"
+      end
+      it 'feeが空だと出品できない' do
+        @item.fee = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Fee can't be blank"
+      end
+      it 'statusが空だと出品できない' do
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Area can't be blank"
+      end
+      it 'dayが空だと出品できない' do
+        @item.day = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Day can't be blank"
+      end
+      it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+      it 'priceが空だと出品できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price is not a number'
+      end
+      it 'priceが300未満だと出品できない' do
+        @item.price = Faker::Number.between(to: 299)
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price must be an integer'
+      end
+      it 'priceが9,999,999より大きいと出品できない' do
+        @item.price = Faker::Number.between(from: 10_000_000)
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price must be an integer'
+      end
+    end
   end
 end
