e7af0a46124647d914c740e725d8440d6ec70b91
Pokesyoum <syoumpm@gmail.com>
2024-09-03 20:15:46 +0900

    正常系テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
+
+  describe '商品出品機能' do
+    context '商品を出品できるとき' do
+      it '必要な情報があれば商品を出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
