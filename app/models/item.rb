04685b4b7b32f7c1ae4a5ba85c585c7f0bccf427
Pokesyoum <syoumpm@gmail.com>
2024-09-03 20:56:06 +0900

    ファイル名とバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,6 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :day
   has_one_attached :image
-  validates :user_id,     presence: true
   validates :item_name,   presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
