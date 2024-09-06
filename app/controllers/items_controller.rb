f04917d80543b18144b70deb76dab0db519601c8
Pokesyoum <syoumpm@gmail.com>
2024-09-06 19:09:53 +0900

    updateアクションとviewファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,11 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to root_path
+    if @item.update
+      redirect_to root_path
+    else
+      render :show, status: :unprocessable_entity
+    end
   end
 
   private
