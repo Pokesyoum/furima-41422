6099ea18f74d967b31bf34da1b8f6af62833ac74
Pokesyoum <syoumpm@gmail.com>
2024-09-08 11:19:58 +0900

    ログインユーザーごとの処理分けの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+  before_action :authenticate_user!
+  before_action :move_to_root
+
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address_form = OrderAddressForm.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @order_address_form = OrderAddressForm.new(order_address_params)
-    @item = Item.find(params[:item_id])
     if @order_address_form.valid?
       pay_item
       @order_address_form.save
@@ -33,5 +35,15 @@ class OrdersController < ApplicationController
       card: order_address_params[:token], # カードトークン
       currency: 'jpy' # 通貨の種類（日本円）
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    return unless current_user.id == @item.user.id
+
+    redirect_to root_path
   end
 end
