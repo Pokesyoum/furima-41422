1812f5264343e5fb1dab16eb32c3905046f26fa0
Pokesyoum <syoumpm@gmail.com>
2024-09-08 19:36:44 +0900

    郵便番号と電話番号のバリデーションの追加

--- app/models/order_address_form.rb
+++ app/models/order_address_form.rb
@@ -3,11 +3,11 @@ class OrderAddressForm
 
   attr_accessor :user_id, :item_id, :token, :postal_code, :area_id, :city, :street, :building, :phone_number
 
-  validates  :postal_code,  presence: true
+  validates  :postal_code,  presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid' }
   validates  :area_id,      numericality: { other_than: 1, message: "can't be blank" }
   validates  :city,         presence: true
   validates  :street,       presence: true
-  validates  :phone_number, presence: true
+  validates  :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
 
   def save
     return false unless valid?
