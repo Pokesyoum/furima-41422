058b53adcac85801053b319604e877d6964439cc
Pokesyoum <syoumpm@gmail.com>
2024-09-08 10:11:29 +0900

    Formオブジェクトを使用したデータの保存（tokenのバリデーションは削除）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,34 +1,37 @@
 class OrdersController < ApplicationController
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @order = Order.new
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @order_address_form = OrderAddressForm.new
     @item = Item.find(params[:item_id])
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_address_form = OrderAddressForm.new(order_address_params)
+    @item = Item.find(params[:item_id])
+    if @order_address_form.valid?
       pay_item
-      @order.save
-      return redirect_to root_path
+      @order_address_form.save
+      redirect_to root_path
     else
-      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render 'index', status: :unprocessable_entity
     end
   end
 
   private
 
-  def order_params
-    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  def order_address_params
+    params.require(:order_address_form).permit(:area_id, :postal_code, :city, :street, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price, # 商品の値段
+      card: order_address_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
     )
   end
 end
--- app/models/order_address_form.rb
+++ app/models/order_address_form.rb
@@ -3,7 +3,6 @@ class OrderAddressForm
 
   attr_accessor :user_id, :item_id, :token, :postal_code, :area_id, :city, :street, :building, :phone_number
 
-  validates  :token,        presence: true
   validates  :postal_code,  presence: true
   validates  :area_id,      numericality: { other_than: 1, message: "can't be blank" }
   validates  :city,         presence: true
@@ -15,22 +14,23 @@ class OrderAddressForm
 
     ActiveRecord::Base.transaction do
       order = Order.create!(
-        user_id: user_id,
-        item_id: item_id
+        user_id:,
+        item_id:
       )
 
       Address.create!(
-        order_id:     order.id,
-        postal_code:  postal_code
-        area_id:      area_id
-        city:         city
-        street:       street
-        building:     building
-        phone_number: phone_number
+        order_id: order.id,
+        postal_code:,
+        area_id:,
+        city:,
+        street:,
+        building:,
+        phone_number:
       )
     end
     true
-  rescue StandardError
+  rescue StandardError => e
+    Rails.logger.error("Failed to save OrderAddressForm: #{e.message}")
     false
   end
 end
