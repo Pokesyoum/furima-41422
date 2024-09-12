fe0d612c992c55b1fd62638391a2eac6db213fdd
Pokesyoum <syoumpm@gmail.com>
2024-09-12 22:32:24 +0900

    レビュー後修正（Formオブジェクトの単体テストコード）

--- spec/factories/order_address_form.rb
+++ spec/factories/order_address_form.rb
@@ -1,11 +1,13 @@
 FactoryBot.define do
-  factory :address do
-    association :order
+  factory :order_address_form do
+    user_id { Faker::Number.number }
+    item_id { Faker::Number.number }
     postal_code { Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4) }
     area_id { Faker::Number.between(from: 2, to: 48) }
     city { Faker::Address.city }
     street { Faker::Address.street_name }
     building { Faker::Address.building_number }
     phone_number { Faker::Number.number(digits: 11) }
+    token { Faker::Alphanumeric.alphanumeric }
   end
 end
---
+++ spec/models/order_address_form_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddressForm, type: :model do
+  before do
+    @order_address_form = FactoryBot.build(:order_address_form)
+  end
+
+  describe '商品購入機能' do
+    context '新規購入できるとき' do
+      it 'すべての情報があれば購入できる' do
+        expect(@order_address_form).to be_valid
+      end
+      it 'buildingがなくても購入できる' do
+        @order_address_form.building = ''
+        expect(@order_address_form).to be_valid
+      end
+    end
+    context '新規購入できないとき' do
+      it 'userが紐づいていないと購入できない' do
+        @order_address_form.user_id = nil
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐づいていないと購入できない' do
+        @order_address_form.item_id = nil
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'postal_codeがないと購入できない' do
+        @order_address_form.postal_code = ''
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeは3桁-4桁の形式でないと購入できない' do
+        @order_address_form.postal_code = '1234567'
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include('Postal code is invalid')
+      end
+      it 'ara_idが1だと購入できない' do
+        @order_address_form.area_id = 1
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'cityがないと購入できない' do
+        @order_address_form.city = ''
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include("City can't be blank")
+      end
+      it 'streetがないと購入できない' do
+        @order_address_form.street = ''
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include("Street can't be blank")
+      end
+      it 'phone_numberがないと購入できない' do
+        @order_address_form.phone_number = ''
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone numberは10または11桁の数字でないと購入できない' do
+        @order_address_form.phone_number = '123456789'
+        @order_address_form.valid?
+        expect(@order_address_form.errors.full_messages).to include('Phone number is invalid')
+      end
+    end
+  end
+end
