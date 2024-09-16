b971783895b2c8264bd8cebdfbc8d9da3c34d161
Pokesyoum <syoumpm@gmail.com>
2024-09-16 14:37:02 +0900

    レビュー後修正3（テストコードのreferences型データの生成）

--- spec/factories/order_address_form.rb
+++ spec/factories/order_address_form.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_address_form do
-    user_id { Faker::Number.number }
-    item_id { Faker::Number.number }
     postal_code { Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4) }
     area_id { Faker::Number.between(from: 2, to: 48) }
     city { Faker::Address.city }
--- spec/models/order_address_form_spec.rb
+++ spec/models/order_address_form_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderAddressForm, type: :model do
   before do
-    @order_address_form = FactoryBot.build(:order_address_form)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address_form = FactoryBot.build(:order_address_form, user_id: user.id, item_id: item.id)
   end
 
   describe '商品購入機能' do
